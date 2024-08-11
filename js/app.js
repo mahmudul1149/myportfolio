b95cb9f2a9fea216f32e83cfff2199960d6a31cb
mahmudul <imdmahmudul237@gmail.com>
2024-08-11 22:24:25 +0600

    update lot's of code on the portfolio

--- js/app.js
+++ js/app.js
@@ -8,3 +8,9 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
+document.getElementById('downloadResume').addEventListener('click', function() {
+  const link = document.createElement('a');
+  link.href = '/images/Mahmudul-Frontend-developer.pdf'; // Replace with the path to your PDF file
+  link.download = 'Mahmudul_Resume.pdf'; // The name of the file once downloaded
+  link.click();
+});
